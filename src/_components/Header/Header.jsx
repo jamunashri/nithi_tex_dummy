78198339a15671c66f52521c73d4c40380d4fb1c
LAKSHMIBALAJI001 <96902774+LAKSHMIBALAJI001@users.noreply.github.com>
2022-05-22 15:32:52 +0530

    Header commit

---
+++ src/_components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './footer.css';
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <p>nithi_tex 2022</p>
+            <div className="social">
+                <i className="fa fa-facebook"></i>
+                <i className="fa fa-instagram"></i>
+            </div>
+        </div>
+    )
+}
+
---
+++ src/_components/Slider/dataSlider.jsx
@@ -0,0 +1,31 @@
+import { v4 as uuidv4 } from "uuid";
+
+const dataSlider = [
+  {
+    id: 1(),
+    title: "",
+    subTitle: ""
+  },
+  {
+    id: 2(),
+    title: "",
+    subTitle: ""
+  },
+  {
+    id: 3(),
+    title: "",
+    subTitle: ""
+  },
+  {
+    id: 4(),
+    title: "",
+    subTitle: ""
+  },
+  {
+    id: 5(),
+    title: "",
+    subTitle: ""
+  },
+];
+
+export default dataSlider;
