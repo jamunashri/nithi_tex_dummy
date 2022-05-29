f22b7d58e8a3bd6a39816fe63b434d6c5fdec4a7
LAKSHMIBALAJI001 <96902774+LAKSHMIBALAJI001@users.noreply.github.com>
2022-05-29 21:06:24 +0530

    listing management

---
+++ src/_components/Product Listing/index.jsx
@@ -0,0 +1,208 @@
+/* import React from 'react'
+import './index.css';
+<html lang="en">
+
+
+<title>Listing Management</title>	
+
+	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+	<link href="css/style.css" rel="stylesheet">
+
+<return></return>
+<body id="page-top">
+
+	<nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+		
+		
+		<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarResponsive">
+
+		</div>
+
+	</nav>
+
+	<header class="page-section masthead2">
+		<div class="container h-20">
+			<h1 class="section-header text-white font-weight-bold">Listing Management</h1>	
+		</div>
+		
+	</header>
+
+	<section class="page-section">
+		<div class="container">
+			<div class="row">
+</div>
+
+				<div class="col-lg-15" style="padding-left: 5px;">
+					<div class="row">
+						<div class="col">
+							Showing all 9 results
+						</div>
+
+						<div class="col">
+							<select class="form-control">
+								<option value="">Sort By</option>
+								<option value="popularity">Popularity</option>
+								<option value="average">Average</option>
+								<option value="latest">Latest</option>
+								<option value="low">Low Price</option>
+								<option value="high">High Price</option>
+							</select>
+						</div>
+
+					</div>
+					<div>&nbsp;</div>
+					<div>&nbsp;</div>
+
+					<div class="row">
+						<div class="col-sm-3 col-md-6 col-lg-4">
+							<div class="card">
+								<div class="card-body text-center">
+									<img src="img/IndianSaree_1.jpg" class="product-image">
+									<h5 class="card-title"><b>High Quality</b></h5>
+									<p class="card-text small">Best collect</p>
+									<p class="tags">Price  Rs.1300</p>
+									<a href="#" class="btn btn-success button-text"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Add to cart</a>
+									</div>
+							</div>
+						</div>
+
+						<div class="col-sm-3 col-md-6 col-lg-4">
+							<div class="card">
+								<div class="card-body text-center">
+									<img src="img/IndianSaree_2.jpg" class="product-image">
+									<h5 class="card-title"><b>High Quality</b></h5>
+									<p class="card-text small">Best collect</p>
+									<p class="tags">Price Rs.1300</p>
+									<a href="#" class="btn btn-success button-text"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Add to cart</a>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-sm-3 col-md-6 col-lg-4">
+							<div class="card">
+								<div class="card-body text-center">
+									<img src="img/IndianSaree_3.jpg" class="product-image">
+									<h5 class="card-title"><b>High Quality</b></h5>
+									<p class="card-text small">Best collect</p>
+									<p class="tags">Price  Rs.1300</p>
+									<a href="#" class="btn btn-success button-text"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Add to cart</a>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				
+					<div>&nbsp;</div>
+					<div>&nbsp;</div>
+
+					<div class="row">
+						<div class="col-sm-3 col-md-6 col-lg-4">
+							<div class="card">
+								<div class="card-body text-center">
+									<img src="img/IndianSaree_1.jpg" class="product-image">
+									<h5 class="card-title"><b>High Quality</b></h5>
+									<p class="card-text small">Best collect</p>
+									<p class="tags">Price  Rs.1300</p>
+									<a href="#" class="btn btn-success button-text"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Add to cart</a>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-sm-3 col-md-6 col-lg-4">
+							<div class="card">
+								<div class="card-body text-center">
+									<img src="img/IndianSaree_2.jpg" class="product-image">
+									<h5 class="card-title"><b>High Quality</b></h5>
+									<p class="card-text small">Best collect</p>
+									<p class="tags">Price  Rs.1300</p>
+									<a href="#" class="btn btn-success button-text"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Add to cart</a>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-sm-3 col-md-6 col-lg-4">
+							<div class="card">
+								<div class="card-body text-center">
+									<img src="img/IndianSaree_3.jpg" class="product-image">
+									<h5 class="card-title"><b>High Quality</b></h5>
+									<p class="card-text small">Best collect</p>
+									<p class="tags">Price Rs.1300</p>
+									<a href="#" class="btn btn-success button-text"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Add to cart</a>
+								</div>
+							</div>
+						</div>
+
+					</div>
+					<div>&nbsp;</div>
+					<div>&nbsp;</div>
+
+					<div class="row">
+						<div class="col-sm-3 col-md-6 col-lg-4">
+							<div class="card">
+								<div class="card-body text-center">
+									<img src="img/IndianSaree_1.jpg" class="product-image">
+									<h5 class="card-title"><b>High Quality</b></h5>
+									<p class="card-text small">Best collect</p>
+									<p class="tags">Price Rs.1300</p>
+									<a href="#" class="btn btn-success button-text"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Add to cart</a>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-sm-3 col-md-6 col-lg-4">
+							<div class="card">
+								<div class="card-body text-center">
+									<img src="img/IndianSaree_2.jpg" class="product-image">
+									<h5 class="card-title"><b>High Quality</b></h5>
+									<p class="card-text small">Best collect</p>
+									<p class="tags">Price Rs.1300</p>
+									<a href="#" class="btn btn-success button-text"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Add to cart</a>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-sm-3 col-md-6 col-lg-4">
+							<div class="card">
+								<div class="card-body text-center">
+									<img src="img/IndianSaree_3.jpg" class="product-image">
+									<h5 class="card-title"><b>High Quality</b></h5>
+									<p class="card-text small">Best collect</p>
+									<p class="tags">Price Rs.1300</p>
+									<a href="#" class="btn btn-success button-text"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Add to cart</a>
+								</div>
+							</div>
+						</div>
+
+					</div>
+			</div>
+		</div>
+	</section>
+
+
+	<div>&nbsp;</div>
+	<div>&nbsp;</div>
+	<div>&nbsp;</div>
+	
+	<div class="row">
+		<div class="col-lg-12">
+			<div class="row boxed">
+				
+				<div class="">
+					<a href="#" class="social-icon"><i class="fa fa-facebook-f"></i></a>
+					<a href="#" class="social-icon"><i class="fa fa-twitter"></i></a>
+					<a href="#" class="social-icon"><i class="fa fa-instagram"></i></a>
+					<a href="#" class="social-icon"><i class="fa fa-google-plus"></i></a>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+ */
