afd80aae7c09ed20ce1f654af68e5d64dd409695
LAKSHMIBALAJI001 <96902774+LAKSHMIBALAJI001@users.noreply.github.com>
2022-05-30 21:56:23 +0530

    table listing commit

--- src/_pages/index.js
+++ src/_pages/index.js
@@ -1,6 +1,18 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom/client';
+import { StyledEngineProvider } from '@mui/material/styles';
+import Demo from './demo';
 import Authentication from './Authentication'
 import Home from './Home'
-export {
+
+export{
     Authentication,
     Home
 }
+ReactDOM.createRoot(document.querySelector("#root")).render(
+  <React.StrictMode>
+    <StyledEngineProvider injectFirst>
+      <Demo />
+    </StyledEngineProvider>
+  </React.StrictMode>
+);
